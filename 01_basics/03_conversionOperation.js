52ca1588cbf031ccd6c7b81f4da0a22844c6d064
Bedant Chhetri <130455420+bedantchh@users.noreply.github.com>
2024-07-15 00:18:22 +0000

    Conversion Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "22"
 
-console.log(typeof score)
-console.log(typeof (score))
+// console.log(typeof score)
+// console.log(typeof (score))
 
 let valueInNumber = Number(score)
 
-console.log(typeof(valueInNumber))
+// console.log(typeof(valueInNumber))
 
 // "22" => 22
 // "22abc" => NaN (but the typeOf NaN is number)
@@ -14,7 +14,7 @@ console.log(typeof(valueInNumber))
 
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1 => true , 0 => false
 // "" => false
@@ -23,5 +23,30 @@ console.log(booleanIsLoggedIn)
 
 let someNumber = 33
 let stringSomeNumber = String(someNumber)
-console.log(stringSomeNumber)
-console.log(typeof(stringSomeNumber))
+// console.log(stringSomeNumber)
+// console.log(typeof(stringSomeNumber))
+
+
+// ********** Operations **********
+
+let value = 12
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(4-2)
+// console.log(2*2)
+// console.log(4/2)
+// console.log(2**2)
+// console.log(6%2)
+
+let str1 = "Bedant"
+let str2 = " Chhetri"
+let str = str1 + str2
+// console.log(str)
+
+
+console.log( 2 + "2")
+console.log( "2" + 2)
+console.log( "2" + 2 + 1)
+console.log( 2 + 2 + "2")
