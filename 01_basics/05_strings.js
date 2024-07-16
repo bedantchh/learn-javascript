f1b6cc7406af107226a401fa1afbd718f031fc90
Bedant Chhetri <130455420+bedantchh@users.noreply.github.com>
2024-07-16 07:32:52 +0000

    String and its methods

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,28 @@
+const name = "Bedant"
+let repoCount = 7
+
+console.log(`Hello! My name is ${name} and my repo count is ${repoCount}. `);
+
+
+const gameName = new String('Bedant-ch')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(4))
+console.log(gameName.indexOf('t'));
+
+
+const newString = gameName.substring(0,3)
+console.log(newString);
+
+const newStringOne = '   bedant   '
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "www.google.com/bedant%20chhetri"
+
+console.log(url.replace('%20','-'));
+console.log(url.includes('bedant'));
