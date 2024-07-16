7be1cfa0ab70fcbb611ddd7a5bbd5ce3eb6139b3
Bedant Chhetri <130455420+bedantchh@users.noreply.github.com>
2024-07-16 17:41:15 +0000

    Maths

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -14,3 +14,20 @@ console.log(otherNumber.toPrecision(3));
 
 const bigNum = 1000000
 console.log(bigNum.toLocaleString('en-IN'))
+
+
+// +++++++++++ Maths +++++++++++++++
+const negative = -99
+// console.log(Math.abs(negative));
+// console.log(Math.round(4.89));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(2,3,1,5,0));
+// console.log(Math.max(2,3,1,5,0));
+
+console.log(Math.floor((Math.random()*10)+1))
+
+const min = 10
+const max = 20
+
+console.log(Math.floor((Math.random() * (max-min + 1)) + min ));
