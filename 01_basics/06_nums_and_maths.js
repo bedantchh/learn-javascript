27b20b4989ff058e7a99621a8ea1cb4127bed827
Bedant Chhetri <130455420+bedantchh@users.noreply.github.com>
2024-07-16 17:13:26 +0000

    Numbers

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,16 @@
+const score = 400
+
+const balance = new Number(100)
+
+console.log(score);
+console.log(balance);
+
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 295.8993
+console.log(otherNumber.toPrecision(3));
+
+const bigNum = 1000000
+console.log(bigNum.toLocaleString('en-IN'))
