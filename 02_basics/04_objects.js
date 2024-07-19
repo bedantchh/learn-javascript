546a657e30d8e0ffa4fef9e03086b7890b21e5b2
Bedant Chhetri <130455420+bedantchh@users.noreply.github.com>
2024-07-19 05:51:00 +0000

    Object methods

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,51 @@
+// const tinderUser = new Object()
+
+const tinderUser = {}
+
+tinderUser.id = "123"
+tinderUser.name = "Sam"
+tinderUser.isLoggedIn = true
+
+// console.log(tinderUser)
+
+const regularUser = {
+    email:"userone@gmail.com",
+    username:{
+        fullusername:{
+            firstname: "bedant",
+            lastname: "chhetri"
+        }
+    }
+}
+// console.log(regularUser.username.fullusername.lastname);
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {4:"a", 3:"b"}
+
+// const obj3 = Object.assign({},obj1,obj2)
+
+const obj3 = {...obj1,...obj2}  // we will use the spread operator to merge two or more objects just like arrays
+
+// console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email : "user1@gmail.com"
+    },
+    {
+        id: 2,
+        email : "user2@gmail.com"
+    },
+    {
+        id: 3,
+        email : "user3@gmail.com"
+    }
+]
+
+// console.log(users[1].email);
+
+console.log(Object.keys(tinderUser))
+console.log(Object.values(tinderUser))
+console.log(Object.entries(tinderUser))
+console.log(tinderUser.hasOwnProperty("isLoggedIn"));
