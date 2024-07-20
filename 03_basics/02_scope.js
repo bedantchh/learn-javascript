ec329c23244a16a2e3376f81d5c8ac8f6521a1f5
Bedant Chhetri <130455420+bedantchh@users.noreply.github.com>
2024-07-20 07:20:54 +0000

    scope level and hositing

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -8,4 +8,34 @@ if(true){
 
 // console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
+
+
+function one (){
+    const name = "bedant"
+    function two (){
+        const lastname = "chhetri"
+        // console.log(name)
+    }
+    // console.log(lastname)
+    two()
+}
+
+one()
+
+
+// ++++++++++++  INTRESTING ++++++++++++++
+
+addone(5)
+function addone (num){
+    return num + 1
+}
+
+
+
+// when we declare a fn like this we cannot call it before the fn 
+addtwo(5)
+
+const addtwo = function(num){
+    return num + 2
+}
