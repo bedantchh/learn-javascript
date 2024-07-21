d6e32f3b016f3cc57b7ecdab0e69135140815927
Bedant Chhetri <130455420+bedantchh@users.noreply.github.com>
2024-07-21 12:40:33 +0000

    while and do while loops

---
+++ 05_iterators/two.js
@@ -0,0 +1,22 @@
+//  While loops
+
+let index=1
+while(index < 10){
+    // console.log(`value is: ${index}`)
+    index+= 2
+}
+
+let myArray = ["one", "two", "three"]
+
+let i = 0
+while(i < myArray.length){
+    // console.log(`value is ${myArray[i]}`)
+    i++
+}
+
+score = 11
+
+do{
+    console.log(`score is ${score}`)
+    score++
+}while(score <= 10)
