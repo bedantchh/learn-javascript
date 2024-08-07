91ea5dba15963cc620f19b1429a9bcd255d28f19
Bedant Chhetri <130455420+bedantchh@users.noreply.github.com>
2024-08-07 18:47:23 +0000

    make your own method

--- 10_OOP_and_classes/prototype.js
+++ 10_OOP_and_classes/prototype.js
@@ -28,3 +28,45 @@ Array.prototype.heyBedant = function(){
 }
 
 myHeros.heyBedant()
+
+
+
+
+// ineheritance
+
+// --Old approach 
+const User = {
+    userName: "Learner",
+    email: "learner@g.com"
+}
+
+
+const Teacher = {
+    makeVideo: true
+}
+
+const TeachingSupport = {
+    isAvailable: false
+}
+
+const TASupport = {
+    makeAssignment : `JS Assignment`,
+    fullTime : true,
+    __proto__ : TeachingSupport
+}
+
+Teacher.__proto__ = User
+
+
+// -- Modern syntax
+Object.getPrototypeOf(TeachingSupport, Teacher)
+
+
+let anotherUserName = "Bed     "
+
+String.prototype.trueLength = function(){
+    console.log(this.trim().length)
+}
+
+"He        ".trueLength()
+anotherUserName.trueLength()
