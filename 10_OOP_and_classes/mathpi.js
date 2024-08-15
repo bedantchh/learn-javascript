dedfa434f8cb32a15b94f7ecc60359a9f8045a0a
Bedant Chhetri <130455420+bedantchh@users.noreply.github.com>
2024-08-15 18:24:37 +0000

    writable, enumerable

--- 10_OOP_and_classes/mathpi.js
+++ 10_OOP_and_classes/mathpi.js
@@ -1,3 +1,31 @@
 const descripter = Object.getOwnPropertyDescriptor(Math,'PI')
 
-console.log(descripter)
+// console.log(descripter)
+
+
+const chai = {
+    name: "ginger chai",
+    price: 250,
+    isAvailable: true,
+
+    oderChai : function(){
+        console.log("Chai nahi bani")
+    } 
+}
+
+
+
+Object.defineProperty(chai, 'name' ,{
+    writable: false,
+    enumerable: false
+})
+
+for (const [key, value] of Object.entries(chai)) {
+
+    if(typeof value !=='function'){
+        console.log(`${key} : ${value}`)
+    }
+}
+
+
+// console.log(Object.getOwnPropertyDescriptor(chai,"name"))
